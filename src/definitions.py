ce10f60d69901983209751e6968e64638abe27ab
aerocyber <aerocyberdev@proton.me>
2023-06-03 15:39:03 +0530

    Changes to UI files

--- src/definitions.py
+++ src/definitions.py
@@ -4,22 +4,22 @@ import os
 import pathlib
 
 class Definitions:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
   
-  def data_dir(self):
-    if os.getenv("XDG_DATA_HOME"):
-      return pathlib.PurePath(os.getenv("XDG_DATA_HOME")).joinpath("sitemarker")
-    else:
-      return pathlib.Path(pathlib.Path.home()).joinpath('.local', 'share', 'sitemarker')
+    def data_dir(self):
+        if os.getenv("XDG_DATA_HOME"):
+            return pathlib.PurePath(os.getenv("XDG_DATA_HOME"))
+        else:
+            return pathlib.Path(pathlib.Path.home()).joinpath('.local', 'share', 'sitemarker')
     
-  def data_path(self):
-    return pathlib.PurePath(self.data_dir()).joinpath('internal.omio')
+    def data_path(self):
+        return pathlib.PurePath(self.data_dir()).joinpath('internal.omio')
   
-  def config_dir(self):
-    if os.getenv("XDG_CONFIG_HOME"):
-      return pathlib.PurePath(os.getenv('XDG_CONFIG_HOME')).joinpath('sitemarker')
-    else:
-      return pathlib.PurePath(pathlib.Path.home()).joinpath('.config', 'sitemarker')
+    def config_dir(self):
+        if os.getenv("XDG_CONFIG_HOME"):
+            return pathlib.PurePath(os.getenv('XDG_CONFIG_HOME'))
+        else:
+            return pathlib.PurePath(pathlib.Path.home()).joinpath('.config', 'sitemarker')
 
 
