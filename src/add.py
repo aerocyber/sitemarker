017a2e83a017bfb58a07e530c28ce6b93d6e6df7
aerocyber <aerocyberdev@proton.me>
2023-06-06 17:18:02 +0530

    Split the functionality of adding a record and displaying error messages from window.py, move the ui of adding a record from Python code to ui files and hence separate ui and logic.

---
+++ src/add.py
@@ -0,0 +1,134 @@
+from gi.repository import Adw
+from gi.repository import Gtk
+from . import ospyata as osmata
+from .ospyata import OspyataException
+import json
+import sys
+from .definitions import Definitions
+
+from .add_error_no_name import AddErrorNoNameWindow
+from .error_window import ErrorWindow
+
+
+def printerr(*args):
+    print(*args, file=sys.stderr)
+
+def printinfo(*args):
+    print(*args, file=sys.stdout)
+
+@Gtk.Template(resource_path='/io/github/aerocyber/sitemarker/add.ui')
+class AddWindow(Adw.Window):
+    __gtype_name__ = 'AddWindow'
+
+    add_entry_name = Gtk.Template.Child()
+    add_entry_url = Gtk.Template.Child()
+    add_category_content_area = Gtk.Template.Child()
+    add_button_okay = Gtk.Template.Child()
+    add_button_cancel = Gtk.Template.Child()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        definitions = Definitions()
+        self.data_path = definitions.data_path()
+        self.db_api = osmata.Osmata()
+        self.add_button_okay.connect('clicked', self.add_record)
+        self.add_button_cancel.connect('clicked', lambda add_button_cancel: self.destroy())
+
+        internal_db_fp = open(str(self.data_path), 'r')
+        _internal_db = json.load(internal_db_fp)
+        internal_db_fp.close()
+
+        _validator_omio = self.db_api.validate_omio(json.dumps(_internal_db))
+
+        if not _validator_omio:
+            # super.err_window(err_title="Invalid Internal DB", err_msg="The internal database is corrupt. Reset it.")
+            _err_win = ErrorWindow(transient_for=self)
+            _err_win.show()
+
+        internal_db = _internal_db
+        # Adding data to the internal db is easy
+        if internal_db != {}:
+            # We don't want to load an empty db as sitemarker's initialization takes care of that.
+            for key in internal_db.keys():
+                try:
+                    _name = key
+                    _url = internal_db[key]["URL"]
+                    _categories = internal_db[key]["Categories"]
+                    self.db_api.push(_name, _url, _categories)
+                except OspyataException:
+                    # This library... loves throwing errors... We must handle it.
+                    # Instead of suppressing it, we will print it to stderr.
+                    # For now.
+                    printerr(OspyataException)
+
+    def add_record(self, add_button_okay):
+
+        # Add the record to db
+        name = self.add_entry_name.get_text()
+        url = self.add_entry_url.get_text()
+        categories_buffer = self.add_category_content_area.get_buffer()
+
+        start_iter = categories_buffer.get_start_iter()
+        end_iter = categories_buffer.get_end_iter()
+        categories = categories_buffer.get_text(start_iter, end_iter, False).split(',')
+
+        for i in range(len(categories)):
+            categories[i] = categories[i].lstrip(' ')
+            categories[i] = categories[i].rstrip(' ')
+            categories[i] = categories[i].lstrip('\t')
+            categories[i] = categories[i].rstrip('\t')
+
+        if not name:
+            _dialog_no_name = ErrorWindow(transient_for=self, message="No name has been entered")
+            _dialog_no_name.show()
+            return
+
+        # URL
+        _is_valid = True
+
+        if name and (not url):
+            _dialog_no_url = ErrorWindow(transient_for=self, message="No URL has been entered")
+            _dialog_no_url.show()
+            return
+
+        try:
+            _is_valid = self.db_api.validate_url(url)
+        except Exception as e:
+            _is_valid = False
+
+        if name and url and (not _is_valid):
+            _dialog_invalid_url = ErrorWindow(transient_for=self, message="Entered URL is invalid")
+            _dialog_invalid_url.show()
+            return
+
+        # Categories
+        if len(categories) == 0:
+            categories = []
+
+        _records = json.loads(self.db_api.dumpOmio())
+        found = 0
+        for key in _records:
+            if key == name:
+                found = 1
+                name_exists_dialog = ErrorWindow(transient_for=self, message="Record with provided name is already present in DB.")
+                name_exists_dialog.show()
+                return
+            if _records[key]["URL"] == url:
+                found = 2
+                url_exists_dialog = ErrorWindow(transient_for=self, message="Record with provided URL is already present in DB.")
+                url_exists_dialog.show()
+                return
+        if found == 0:
+            if not (name == '' or url == ''):
+                self.db_api.push(name, url, categories)
+                self.save_records()
+                self.add_dialog.destroy()
+    def save_records(self):
+        # Save the records.
+
+        # We are replacing the existing database.
+        # This is by design because as new data is being entered,
+        # the old database becomes outdated.
+        f = open(str(self.data_path), 'w')
+        f.write(self.db_api.dumpOmio())
+        f.close()
--- src/window.py
+++ src/window.py
@@ -36,10 +36,16 @@ import sys
 import json
 import webbrowser
 import os
+from .definitions import Definitions
 
+from .add import AddWindow
+from .error_window import ErrorWindow
 
 def printerr(*args):
     print(*args, file=sys.stderr)
+
+def printinfo(*args):
+    print(*args, file=sys.stdout)
 
 
 @Gtk.Template(resource_path='/io/github/aerocyber/sitemarker/window.ui')
@@ -65,23 +71,17 @@ class SitemarkerWindow(Adw.ApplicationWindow):
         # The DB interface
         self.db_api = osmata.Osmata()
 
-        # Check if DB exist in application's default (data) location
-        # The dot in front of sitemarker is because we want to create a hidden directory
-        # in *nix systems. It doesn't have any effect on Windows, but, the *nix
-        # system has a default layout for $HOME. We don't want sitemarker to break that.
-        if not os.getenv("XDG_DATA_HOME"):
-            data_dir = pathlib.PurePath.joinpath(pathlib.Path.home(), '.local', 'share', 'sitemarker')
-        else:
-            data_dir = pathlib.Path(os.getenv("XDG_DATA_HOME"))
+        definitions = Definitions()
+        data_dir = definitions.data_dir()
 
 
         # The internal.omio file is just as valid as the original file.
         # This is by design because, if for some reason the application does not work
         # all of a sudden, we do not want the data to be unrecoverable.
         # Copying internal.omio file is just enough to get the data back.
-        self.data_path = pathlib.PurePath.joinpath(data_dir, 'internal.omio')
-        if not pathlib.Path.exists(self.data_path):
-            pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
+        self.data_path = definitions.data_path()
+        if not pathlib.Path.exists(pathlib.Path(self.data_path)):
+            pathlib.Path(str(data_dir)).mkdir(parents=True, exist_ok=True)
             f = open(str(self.data_path), 'w')
             f.write('{}')
             f.close()
@@ -91,7 +91,8 @@ class SitemarkerWindow(Adw.ApplicationWindow):
         internal_db_fp.close()
         _validator_omio = self.db_api.validate_omio(json.dumps(_internal_db))
         if not _validator_omio:
-            self.err_window(err_title="Invalid Internal DB", err_msg="The internal database is corrupt. Reset")
+            err_window = ErrorWindow(transient_for=self, message="The internal database is corrupt.")
+            err_window.show()
         internal_db = _internal_db
         # Adding data to the internal db is easy
         if internal_db != {}:
@@ -110,134 +111,8 @@ class SitemarkerWindow(Adw.ApplicationWindow):
 
 
     def on_add_action(self, widget, _):
-        # The Add action dialog box
-
-        # The Window is the central part of this section.
-        # It is supposed to contain all the other widgets.
-        self.add_dialog = Adw.Window()
-        self.add_dialog.set_transient_for(self) # For making it a modal above the main Application Window
-        self.add_dialog.set_default_size(550, 400) # Default size
-
-        # The box that has it all. All child widgets are made under this box.
-        # This box is made the child of the Window
-        add_dialog_box = Gtk.Box()
-        # We don't want a cursed ui now, do we?
-        # Gtk.Orientation.VERTICAL places each items vertically and the
-        # set_orientation is used to set this behaviour to the Window.
-        add_dialog_box.set_orientation(Gtk.Orientation.VERTICAL)
-
-        add_header = Adw.HeaderBar() # Our header bar
-        self.add_dialog.set_title("Add a record")
-        add_dialog_box.append(add_header) # HeaderBar into the container
-
-        self.add_dialog.set_content(add_dialog_box) # The content of the Window is the container box.
-
-        # The add_box is where the layout is actually gonna go.
-        # This is again, appended to the add_dialog_box
-        add_box = Gtk.Box()
-        add_box.set_orientation(Gtk.Orientation.VERTICAL) # Vertical alignment... again.
-        add_box.set_margin_start(20)
-        add_box.set_margin_end(20)
-        add_box.set_margin_top(20)
-        add_box.set_margin_bottom(20)
-        add_dialog_box.append(add_box) # Add the box to the main box.
-
-        # The layout of the add_box
-
-        # Name
-        add_box_name = Gtk.Box()
-        add_box_name.set_margin_top(20)
-        add_box_name.set_margin_bottom(20)
-        add_box_name.set_orientation(Gtk.Orientation.HORIZONTAL)
-        add_box_name.set_spacing(10)
-
-        add_label_name = Gtk.Label()
-        add_label_name.set_text("Name")
-        add_box_name.append(add_label_name)
-
-        self.add_entry_name = Gtk.Entry()
-        self.add_entry_name.set_alignment(0.01)
-        self.add_entry_name.set_input_hints(Gtk.InputHints.SPELLCHECK)
-        self.add_entry_name.set_placeholder_text("Enter the Name")
-        self.add_entry_name.set_max_width_chars(100)
-        add_box_name.append(self.add_entry_name)
-
-        add_box.append(add_box_name)
-
-        # URL
-        add_box_url = Gtk.Box()
-        add_box_url.set_margin_top(20)
-        add_box_url.set_margin_bottom(20)
-        add_box_url.set_orientation(Gtk.Orientation.HORIZONTAL)
-
-        add_label_url = Gtk.Label()
-        add_label_url.set_text("URL")
-        add_box_url.append(add_label_url)
-        add_box_url.set_spacing(20)
-
-        self.add_entry_url = Gtk.Entry()
-        self.add_entry_url.set_alignment(0.01)
-        self.add_entry_url.set_max_width_chars(100)
-        self.add_entry_url.set_placeholder_text("Enter the URL")
-        self.add_entry_url.set_input_hints(Gtk.InputHints.LOWERCASE)
-        add_box_url.append(self.add_entry_url)
-
-        add_box.append(add_box_url)
-
-        # Categories
-        add_box_categories = Gtk.Box()
-        add_box_categories.set_orientation(Gtk.Orientation.VERTICAL)
-
-        add_label_category = Gtk.Label()
-        add_label_category.set_text("Tags. Separate them with comma(s).")
-        add_box_categories.append(add_label_category)
-        add_box_categories.set_spacing(10)
-
-        # Before going to the content area, a scrolling area is defined to contain it.
-        # So that users can scroll, if there are a lot of categories, even when
-        # it isn't going to happen. But hey! It's humans!
-        add_category_scrolled_window = Gtk.ScrolledWindow.new()
-        add_category_scrolled_window.set_max_content_height(300)
-        add_category_scrolled_window.set_max_content_width(200)
-        add_category_scrolled_window.set_min_content_height(200)
-        add_category_scrolled_window.set_min_content_width(100)
-        add_category_scrolled_window.set_margin_start(10)
-        add_category_scrolled_window.set_margin_end(10)
-
-        self.add_category_content_area = Gtk.TextView()
-        self.add_category_content_area.set_monospace(True)
-
-        # All the category related addition
-        add_category_scrolled_window.set_child(self.add_category_content_area)
-        add_box_categories.append(add_category_scrolled_window)
-        add_box.append(add_box_categories)
-
-        # Buttons!
-
-        # The Button container
-        add_box_button = Gtk.Box()
-        add_box_button.set_margin_top(20)
-        add_box_button.set_margin_bottom(20)
-        add_box_button.set_margin_start(200)
-        add_box_button.set_margin_end(200)
-        add_box_button.set_orientation(Gtk.Orientation.HORIZONTAL)
-        add_box_button.set_spacing(10)
-        add_dialog_box.append(add_box_button)
-
-        # The Add button
-        add_button_okay = Gtk.Button()
-        add_button_okay.set_label("Add Record")
-        add_box_button.append(add_button_okay)
-        add_button_okay.connect('clicked', self.add_record)
-
-        # The Cancel button
-        add_button_cancel = Gtk.Button()
-        add_button_cancel.set_label("Cancel")
-        add_box_button.append(add_button_cancel)
-        add_button_cancel.connect('clicked', lambda add_button_cancel: self.add_dialog.destroy())
-
-        # Show it!
-        self.add_dialog.show()
+        add_win = AddWindow()
+        add_win.show()
 
     def on_view_action(self, widget, _):
         # Update the code to view all records.
@@ -293,7 +168,7 @@ class SitemarkerWindow(Adw.ApplicationWindow):
             self.del_list.append(key)
         self.save_records()
         _tmp_win = Adw.Window()
-        self.err_window(err_title="Deleted successfully", err_msg=f"{self.selected_to_del} has been successfully deleted and the records saved locally.")
+        self.err_window(transient_for=self, err_title="Deleted successfully", err_msg=f"{self.selected_to_del} has been successfully deleted and the records saved locally.")
 
     def on_to_del_selected(self, dropdown, _):
         # Selected Gtk.StringObject
@@ -329,156 +204,6 @@ class SitemarkerWindow(Adw.ApplicationWindow):
         f = open(str(self.data_path), 'w')
         f.write(self.db_api.dumpOmio())
         f.close()
-
-
-    def add_record(self, add_button_okay):
-        # Add the record to db
-        name = self.add_entry_name.get_text()
-        url = self.add_entry_url.get_text()
-        categories_buffer = self.add_category_content_area.get_buffer()
-        start_iter = categories_buffer.get_start_iter()
-        end_iter = categories_buffer.get_end_iter()
-        categories = categories_buffer.get_text(start_iter, end_iter, False).split(',')
-        for i in range(len(categories)):
-            categories[i] = categories[i].lstrip(' ')
-            categories[i] = categories[i].rstrip(' ')
-            categories[i] = categories[i].lstrip('\t')
-            categories[i] = categories[i].rstrip('\t')
-
-        if not name:
-            _dialog_no_name = Adw.Window(transient_for=self.add_dialog)
-            _dialog_no_name.set_default_size(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_no_name.set_size_request(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_no_name.set_title("Error")
-            _dialog_no_name.set_modal(True)
-            _dialog_no_name_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            _dialog_no_name.set_content(_dialog_no_name_box)
-
-            _dialog_no_name_box.append(Adw.HeaderBar())
-
-            _dialog_no_name_box.append(
-                Gtk.Label(label="No name has been entered", margin_bottom=int(768 / 6), margin_end=10,
-                          margin_start=10, margin_top=int(768 / 6)))
-            _dialog_no_name.show()
-            return
-
-        # URL
-        _is_valid = True
-
-        if name and (not url):
-            _dialog_no_url = Adw.Window(transient_for=self.add_dialog)
-            _dialog_no_url.set_default_size(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_no_url.set_size_request(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_no_url.set_title("Error")
-            _dialog_no_url.set_modal(True)
-            _dialog_no_url_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            _dialog_no_url.set_content(_dialog_no_url_box)
-
-            _dialog_no_url_box.append(Adw.HeaderBar())
-
-            _dialog_no_url_box.append(
-                Gtk.Label(label="No url has been entered", margin_bottom=int(768 / 6), margin_end=10,
-                          margin_start=10, margin_top=int(768 / 6)))
-            _dialog_no_url.show()
-            return
-
-        try:
-            _is_valid = self.db_api.validate_url(url)
-        except Exception as e:
-            _is_valid = False
-
-        if name and url and (not _is_valid):
-            _dialog_invalid_url = Adw.Window(transient_for=self.add_dialog)
-            _dialog_invalid_url.set_default_size(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_invalid_url.set_size_request(width=int(1366 / 5), height=int(768 / 3))
-            _dialog_invalid_url.set_title("Error")
-            _dialog_invalid_url.set_modal(True)
-            _dialog_invalid_url_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            _dialog_invalid_url.set_content(_dialog_invalid_url_box)
-            _dialog_invalid_url_box.append(Adw.HeaderBar())
-            _dialog_invalid_url_box.append(
-                Gtk.Label(label="Entered URL is invalid", margin_bottom=int(768 / 6), margin_end=10,
-                          margin_start=10, margin_top=int(768 / 6)))
-            _dialog_invalid_url.show()
-            return
-
-        # Categories
-        if len(categories) == 0:
-            categories = []
-
-        _records = json.loads(self.db_api.dumpOmio())
-        found = 0
-        for key in _records:
-            if key == name:
-                found = 1
-                name_exists_dialog = Adw.Window(transient_for=self)
-                name_exists_dialog.set_modal(True)
-
-                no_name_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-                name_exists_dialog.set_content(no_name_box)
-                name_exists_dialog.set_title("Error")
-                no_name_box.append(Adw.HeaderBar())
-                name_exists_dialog.set_default_size(width=int(1366 / 5), height=int(768 / 3))
-                name_exists_dialog.set_size_request(width=int(1366 / 5), height=int(768 / 3))
-                no_name_box.append(Gtk.Label(label="Record with provided Name is already present in DB.",
-                                             margin_top=10, margin_start=10, margin_end=10, margin_bottom=10))
-                name_exists_dialog.set_modal(True)
-                name_exists_dialog.show()
-                return
-            if _records[key]["URL"] == url:
-                found = 2
-                url_exists_dialog = Adw.Window(transient_for=self)
-                url_exists_dialog.set_modal(True)
-                no_url_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-                url_exists_dialog.set_content(no_url_box)
-                no_url_box.append(Adw.HeaderBar())
-                url_exists_dialog.set_title("Error")
-                url_exists_dialog.set_default_size(width=int(1366 / 5), height=int(768 / 3))
-                url_exists_dialog.set_size_request(width=int(1366 / 5), height=int(768 / 3))
-                no_url_box.append(Gtk.Label(label="Record with provided URL is already present in DB.",
-                                            margin_top=10, margin_start=10, margin_end=10, margin_bottom=10))
-                url_exists_dialog.set_modal(True)
-                url_exists_dialog.show()
-                return
-        if found == 0:
-            if not (name == '' or url == ''):
-                self.db_api.push(name, url, categories)
-                self.save_records()
-                self.add_dialog.destroy()
-
-
-    def err_window(self, err_title="Error", err_msg="Sorry, internal error"):
-        error_dialog = Adw.Window()
-        error_dialog.set_default_size(
-            height=int(768 / 3),
-            width=int(1366 / 5)
-        )
-        error_dialog.set_transient_for(self)
-        error_dialog.set_title(err_title)
-        layout = Gtk.Box()
-        layout.set_orientation(Gtk.Orientation.VERTICAL)
-        layout.append(Adw.HeaderBar())
-        # layout.set_margin_top(20)
-        layout.set_margin_bottom(20)
-        # layout.set_margin_start(10)
-        # layout.set_margin_end(10)
-        layout.append(
-            Gtk.Label(label=err_msg,
-            margin_top=int(1366 / 20),
-            margin_start=10,
-            margin_end=10,
-            margin_bottom=20
-            )
-        )
-        close_btn = Gtk.Button(label="Okay")
-        close_btn.set_margin_top(10)
-        close_btn.set_margin_bottom(10)
-        close_btn.set_margin_start(10)
-        close_btn.set_margin_end(10)
-        close_btn.connect('clicked', lambda close_btn: error_dialog.close())
-        layout.append(close_btn)
-        error_dialog.set_content(layout)
-        error_dialog.show()
 
     def view_element(self, widget, db, win_title="View Records"):
         # View all records from db.
@@ -629,7 +354,8 @@ class SitemarkerWindow(Adw.ApplicationWindow):
                     db = osmata.Osmata().loadOmio(final_filenames[0])
                 except Exception:
                     printerr("Invalid file")
-                    self.err_window(err_title="Error", err_msg="Invalid File")
+                    err_window = ErrorWindow(transient_for=self, message="Invalid File")
+                    err_window.show()
                     return
 
 
@@ -825,7 +551,8 @@ class SitemarkerWindow(Adw.ApplicationWindow):
                     db = osmata.Osmata().loadOmio(final_filenames[0])
                 except Exception:
                     printerr("Invalid file")
-                    self.err_window(err_title="Error", err_msg="Invalid File")
+                    err_window = ErrorWindow(transient_for=self, message="Invalid File")
+                    err_window.show()
                     return
 
 
