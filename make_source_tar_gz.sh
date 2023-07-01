62947b908e9a614a8bafb6830dfcbd6e99bb8c38
Aero <aerocyberdev@proton.me>
2023-07-01 10:26:33 +0530

    Update version in make_source_tar_gz.sh

--- make_source_tar_gz.sh
+++ make_source_tar_gz.sh
@@ -1,4 +1,4 @@
-export VERSION="1.2.0"
+export VERSION="1.2.2"
 export SRC_DIR="sitemarker-$VERSION"
 mkdir $SRC_DIR
 cp -r ./data ./docs ./po ./src ./io.github.aerocyber.sitemarker.json ./README.md ./$SRC_DIR
