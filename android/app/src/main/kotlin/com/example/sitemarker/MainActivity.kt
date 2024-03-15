28e8bb08dc3b44b02263755a4536af3311581974
Aero <aerocyberdev@proton.me>
2024-03-15 15:38:53 +0530

    Do desktop file and appdata.xml file for linux. Chnage and update the icon.

---
+++ android/app/src/main/kotlin/com/example/sitemarker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sitemarker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/io/github/aerocyber/sitemarker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.aerocyber.sitemarker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
