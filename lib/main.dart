36e55233cd40420b88cf00d42ef2098e65c8f1ef
Aero <aerocyberdev@proton.me>
2024-04-30 22:19:25 +0530

    Remove unwanted dependency import.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:sitemarker/data/dbrecord_provider.dart';
 import 'package:sitemarker/pages/page_settings.dart';
