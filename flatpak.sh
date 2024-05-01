830ae856eca1b3bce1b65d4c3b00d85c26888cd3
Aero <aerocyberdev@proton.me>
2024-05-01 09:54:06 +0530

    Update flatpak build

--- flatpak.sh
+++ flatpak.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/bash
 
 # Set up Flutter
 # Get flutter
@@ -16,13 +16,38 @@ cd sitemarker
 # Build it
 flutter pub get
 flutter build linux --tree-shake-icons --release --build-name=$VERSION --split-debug-info=$VERSION-linux --no-obfuscate
-# Fix linking errors
 
 # Packaging
 
 # Check if build successful
 cd build/linux/x64/release/bundle || exit 1
 
-# Convert it to .tar.gz archive
-tar -czaf $archiveName ./*
-mv $archiveName "$baseDir"/
+# Fix linking errors
+cd lib
+patchelf --set-rpath '$ORIGIN' libsqlite3_flutter_libs_plugin.so
+patchelf --set-rpath '$ORIGIN' liburl_launcher_linux_plugin.so
+
+# Copy it
+cd ..
+mkdir -p /app/sitemarker
+cp -r ./* /app/sitemarker
+chmod +x /app/sitemarker/sitemarker
+ln -s /app/sitemarker /app/bin/sitemarker
+
+# Move back to project home
+cd ../../../../..
+
+# Install the icon.
+iconDir=/app/share/icons/hicolor/scalable/apps
+mkdir -p $iconDir
+cp -r assets/icons/$projectId.svg $iconDir/
+
+# Install the desktop file.
+desktopFileDir=/app/share/applications
+mkdir -p $desktopFileDir
+cp -r packaging/linux/$projectId.desktop $desktopFileDir/
+
+# Install the AppStream metadata file.
+metadataDir=/app/share/metainfo
+mkdir -p $metadataDir
+cp -r packaging/linux/$projectId.metainfo.xml $metadataDir/
