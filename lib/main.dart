980ca4131a86140b2ff353e2ef819537e4f272dc
Aero <aerocyberdev@proton.me>
2024-05-30 06:59:12 +0530

    Bugfix: Show donate dialog once per day.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
-import 'dart:math' show Random;
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:sitemarker/data/dbrecord_provider.dart';
 import 'package:sitemarker/pages/page_settings.dart';
@@ -8,6 +10,7 @@ import 'package:sitemarker/pages/page_view.dart';
 import 'package:sitemarker/color_schemes.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import 'package:universal_io/io.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -109,42 +112,70 @@ class _SitemarkerHomeState extends State<SitemarkerHome> {
     );
   }
 
-  void _donateNotify() {
-    var ran = Random();
-    randomness ??= ran.nextInt(100);
+  Future<Directory> getAppDir() async {
+    if (Platform.isLinux) {
+      return Directory(Platform.environment["XDG_DATA_DIR"] ??
+          "${Platform.environment["HOME"]}/.config/sitemarker");
+    } else {
+      return await getApplicationSupportDirectory();
+    }
+  }
+
+  void _donateNotify() async {
+    bool showDonate = true;
+    if (!kIsWeb) {
+      Directory dir = await getAppDir();
+      File f = File("${dir.path.toString()}/donate_show.timestamp");
+      if (!f.existsSync()) {
+        f.createSync(recursive: true);
+        f.writeAsStringSync(
+            "${DateTime.timestamp().day}-${DateTime.timestamp().month}-${DateTime.timestamp().year}");
+      } else {
+        var timestamp = f.readAsStringSync();
+        if (timestamp ==
+            "${DateTime.timestamp().day}-${DateTime.timestamp().month}-${DateTime.timestamp().year}") {
+          showDonate = false;
+        } else {
+          f.writeAsStringSync(
+              "${DateTime.timestamp().day}-${DateTime.timestamp().month}-${DateTime.timestamp().year}");
+        }
+      }
+    }
     String bmc = "https://buymeacoffee.com/aerocyber";
     String ghs = "https://github.com/sponsors/aerocyber";
-    if (randomness! >= showDonate) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Support the project"),
-            content: const Text(
-                "This project is powered by awesome folks like you! Please donate to support the project's development! We support 2 ways of donations! Choose either of these."),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  launchUrl(Uri.parse(bmc));
-                },
-                child: const Text("Buy Me a Coffee"),
-              ),
-              TextButton(
-                onPressed: () {
-                  launchUrl(Uri.parse(ghs));
-                },
-                child: const Text("Github Sponsors"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Remind me later"),
-              )
-            ],
-          );
-        },
-      );
+    if (showDonate) {
+      if (mounted) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Support the project"),
+              content: const Text(
+                  "This project is powered by awesome folks like you! Please donate to support the project's development! We support 2 ways of donations! Choose either of these."),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse(bmc));
+                  },
+                  child: const Text("Buy Me a Coffee"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse(ghs));
+                  },
+                  child: const Text("Github Sponsors"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Remind me later"),
+                )
+              ],
+            );
+          },
+        );
+      }
     }
   }
 
