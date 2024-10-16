c033bcde4d8ff488bf641b6522deed8f24064d54
Aero <aerocyberdev@proton.me>
2024-10-16 23:00:34 +0530

    Add db layer

--- sitemarker/lib/core/data/db/db_access.dart
+++ sitemarker/lib/core/data/db/db_access.dart
@@ -12,3 +12,67 @@ class SitemarkerRecords extends Table {
 }
 
 @DriftDatabase(tables: [SitemarkerRecords])
+class SitemarkerDB extends _$SitemarkerDB {
+  SitemarkerDB() : super(impl.connect());
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<List<SitemarkerRecord>> get allRecords =>
+      select(sitemarkerRecords).get();
+
+  Future<List<SitemarkerRecord>> getRecordsByName(String name) {
+    return (select(sitemarkerRecords)..where((t) => t.name.equals(name))).get();
+  }
+
+  Future<List<SitemarkerRecord>> getRecordsByTags(List<String> tags) async {
+    List<SitemarkerRecord> toRet = [];
+    List<SitemarkerRecord> tmp = [];
+    List<SitemarkerRecord> srl = await select(sitemarkerRecords).get();
+
+    for (int i = 0; i < srl.length; i++) {
+      List<String> s = srl[i].tags.split(",");
+      for (int j = 0; i < s.length; j++) {
+        String str = s[i].trim();
+        s[i] = str;
+      }
+      tmp.add(SitemarkerRecord(
+        id: srl[i].id,
+        name: srl[i].name,
+        url: srl[i].url,
+        tags: s.join(","),
+        isDeleted: false,
+      ));
+    }
+
+    for (int i = 0; i < tmp.length; i++) {
+      List<String> tmpTags = tmp[i].tags.split(',');
+      bool ignoreIter = false;
+      for (int t = 0; t < tags.length; t++) {
+        if (!tmpTags.contains(tags[i])) {
+          tmp.removeAt(i);
+          ignoreIter = true;
+        }
+      }
+      if (ignoreIter) {
+        break;
+      }
+    }
+
+    toRet = tmp;
+
+    return toRet;
+  }
+
+  // INSERT
+  Future<int> insertRecord(SitemarkerRecord record) =>
+      into(sitemarkerRecords).insert(record);
+
+  // UPDATE
+  Future<bool> updateRecord(SitemarkerRecord record) =>
+      update(sitemarkerRecords).replace(record);
+
+  // DELETE
+  Future<int> deleteRecord(SitemarkerRecord record) =>
+      delete(sitemarkerRecords).delete(record);
+}
