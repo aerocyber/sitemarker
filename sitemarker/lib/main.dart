80225a47840151407eae0c7e7ea8dc81a7615d2c
Aero <aerocyberdev@proton.me>
2024-12-08 07:34:33 +0530

    DIR structure changes, purge unwanted files

--- sitemarker/lib/main.dart
+++ sitemarker/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sitemarker/ui/pages/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
