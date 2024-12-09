fd08bd9f87dadf020c0424ce06c8386715536b94
Aero <aerocyberdev@proton.me>
2024-12-09 11:35:22 +0530

    Add data helper class Add notifier stuff from 2.x

---
+++ sitemarker/lib/core/data_types/userdata/sm_record.dart
@@ -0,0 +1,7 @@
+class SmRecord {
+  String name;
+  String url;
+  String tags;
+
+  SmRecord({required this.name, required this.url, required this.tags});
+}
---
+++ sitemarker/lib/core/db/smdb_provider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:sitemarker/core/data_types/userdata/sm_record.dart';
+import 'package:sitemarker/core/db/sqlitedb/sm_db.dart';
+
+class SmdbProvider extends ChangeNotifier {
+  late SitemarkerDB db;
+  List<SitemarkerRecord> _records = [];
+  List<SitemarkerRecord> get records => _records;
+  List<SitemarkerRecord> deletedRecords = [];
+  List<SitemarkerRecord> allRecords = [];
+
+  /// Load the values from db. Not to be called from ooutside the class
+  void init() async {
+    List<SitemarkerRecord> recs = await db.allRecords;
+    for (int i = 0; i < recs.length; i++) {
+      if (recs[i].isDeleted) {
+        deletedRecords.add(recs[i]);
+      } else {
+        _records.add(recs[i]);
+      }
+    }
+    allRecords = recs;
+    notifyListeners();
+  }
+
+  SmdbProvider() {
+    db = SitemarkerDB();
+    init();
+  }
+
+  /// Get the default id to be used. Called internally.
+  int getDefualtId() {
+    if (_records.isNotEmpty) {
+      return _records.last.id + 1;
+    }
+    return 0;
+  }
+
+  void insertRecord(SmRecord record) async {
+    final rec = SitemarkerRecord(
+      id: getDefualtId(),
+      name: record.name,
+      url: record.url,
+      tags: record.tags,
+      isDeleted: false,
+      dateAdded: DateTime(
+        DateTime.now().year,
+        DateTime.now().month,
+        DateTime.now().day,
+      ),
+    );
+    _records.add(rec);
+    db.insertRecord(rec);
+    notifyListeners();
+  }
+
+  void deleteRecord(SmRecord record) async {
+    final rec = await db.getRecordsByName(record.name);
+    db.softDelete(rec.first);
+    _records.remove(rec.first);
+    notifyListeners();
+  }
+
+  void updateRecord(SitemarkerRecord record) async {
+    db.updateRecord(record);
+    _records = await db.allRecords;
+    notifyListeners();
+  }
+}
