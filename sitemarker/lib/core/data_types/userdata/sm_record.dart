bd7c75479b0782185b4b032eaabe7acac9f74f29
Aero <aerocyberdev@proton.me>
2025-12-09 09:30:29 +0530

    Add notes field to SmRecord as optional

--- sitemarker/lib/core/data_types/userdata/sm_record.dart
+++ sitemarker/lib/core/data_types/userdata/sm_record.dart
@@ -6,10 +6,12 @@ class SmRecord {
   String tags;
   DateTime dt;
   bool? isDeleted;
+  String? notes;
 
   SmRecord({
     this.id,
     this.isDeleted,
+    this.notes,
     required this.name,
     required this.url,
     required this.tags,
