2b4494e430d5d1948c1ded4f3effd4aa8c79d86a
Aero <aerocyberdev@proton.me>
2026-06-06 00:59:14 +0530

    Remove backend from this repo

--- backend/run.sh
+++
@@ -1,6 +0,0 @@
-gunicorn wsgi:app \
-  --workers=4 \
-  --bind=0.0.0.0:8000 \
-  --log-level=info \
-  --access-logfile=/var/log/sitemarker/logs.log \
-  --error-logfile=/var/log/sitemarker/errors.log
